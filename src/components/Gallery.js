a485eb4f90bfdcd4d2280004b4380e818e62c281
ecbob <chestercervantes13@hotmail.com>
2020-08-01 13:35:56 -0700

    Add gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,34 +1,27 @@
-import React from 'react';
-import KitsGallery from './KitsGallery.js';
+import React, { Component} from 'react';
+import Tile from './Tile.js';
+import InfoSection from './InfoSection.js';
 
-
-function Gallery({ imgarr }) {
-
-    return (
-        <div className="gallery">
-
-            <table>
-                <td>
-                    
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-                <td>
-
-                </td>
-            </table>
-        </div>
-    );
+class Gallery extends Component {
+	this() {
+		console.log(this.props.name)
+		return (
+			<InfoSection 
+				key={this.props.id}
+				title={this.props.name}
+			/>
+			)
+	}
+	render() {
+		// Create tile for each item in data array
+		// Pass data to each tile and assign a key
+		return (
+			<div className="tiles">
+				{this.props.data.map((data) => {
+					return <Tile data={data} key={data.id} />
+				})}
+			</div>
+		);
+	}
 }
-
 export default Gallery;
--- src/components/InfoSectionTitles.js
+++ src/components/InfoSectionTitles.js
@@ -10,15 +10,26 @@ const infoSectionTitles = [
 		type: "image",
 		title: "Kits",
 		images: [
-		{
-			img: "./cocoaqua.jpg"
-		},
-		{
-			img: "./cocoaqua.jpg"
-		},
-		{
-			img: "./images/cocoaqua.jpg"
-		}
+			{
+				id: 1,
+				name: "Coco1",
+				image: "https://images.unsplash.com/photo-1445723356089-6dbb51d9c4f8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=6e476c6e7ce1adac161295616d1bec05"
+			},
+			{
+				id: 2,
+				name: "Coco2",
+				image: "https://images.unsplash.com/photo-1445723356089-6dbb51d9c4f8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=6e476c6e7ce1adac161295616d1bec05"
+			},
+			{
+				id: 3,
+				name: "Coco3",
+				image: "https://images.unsplash.com/photo-1445723356089-6dbb51d9c4f8?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=980&h=980&fit=crop&s=6e476c6e7ce1adac161295616d1bec05"
+			},
+			{
+				id: 8,
+				name: "Garden",
+				image: "https://images.unsplash.com/photo-1427392797425-39090deb14ec?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=900&h=900&fit=crop&s=8bfe49466d0da200e61128a8ab0e8fbe"
+			}
 		]
 	},
 	{
