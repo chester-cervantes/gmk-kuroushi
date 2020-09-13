5425668fea0b8d859d1cb03520b684b32ea9b2fc
ecbob <chestercervantes13@hotmail.com>
2020-09-12 19:31:14 -0700

    - edit css files - add social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,8 @@ import './Footer.css';
 function Footer() {
     return (
         <div className="footer">
-            Follow Us!
+
+            <h2>Follow Our Social Medias!</h2>
             <table>
                 <tr>
                     <td>
@@ -20,10 +21,10 @@ function Footer() {
                         </a>
                     </td>
                     <td>
-                        <a href="https://www.instagram.com/chester.cervantes/">
+                        <a href="https://discord.gg/PbvMNrr">
                             <img
                                 className="social-media-icons"
-                                src="glyph-logo_May2016.png"
+                                src="discord.png"
                                 style={{
                                     height: 30,
                                     width: 30
@@ -32,7 +33,14 @@ function Footer() {
                         </a>
                     </td>
                 </tr>
+
             </table>
+            <div className="social-media-icons-attribution">
+                Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+            </div>
+            <div className="social-media-icons-attribution">
+                Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+            </div>
         </div>
     );
 }
