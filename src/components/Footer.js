b13417165129e93c926cf0573f3931ff38b295b9
ecbob <chestercervantes13@hotmail.com>
2020-09-12 19:32:41 -0700

    - edit so social media links open in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,10 +9,10 @@ function Footer() {
             <table>
                 <tr>
                     <td>
-                        <a href="https://www.instagram.com/chester.cervantes/">
+                        <a href="https://www.instagram.com/chester.cervantes/" target="_blank">
                             <img
                                 className="social-media-icons"
-                                src="glyph-logo_May2016.png"           
+                                src="glyph-logo_May2016.png"
                                 style={{
                                     height: 30,
                                     width: 30
@@ -21,7 +21,7 @@ function Footer() {
                         </a>
                     </td>
                     <td>
-                        <a href="https://discord.gg/PbvMNrr">
+                        <a href="https://discord.gg/PbvMNrr" target="_blank">
                             <img
                                 className="social-media-icons"
                                 src="discord.png"
@@ -36,10 +36,12 @@ function Footer() {
 
             </table>
             <div className="social-media-icons-attribution">
-                Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+                Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel
+                perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
             </div>
             <div className="social-media-icons-attribution">
-                Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+                Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a
+                href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
             </div>
         </div>
     );
