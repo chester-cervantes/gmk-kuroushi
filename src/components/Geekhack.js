3fc77ec3bf2c596f333361d48671bd959329f35e
ecbob <chestercervantes13@hotmail.com>
2020-11-19 01:28:55 -0800

    - change geekhack to <a> tag

--- src/components/Geekhack.js
+++ src/components/Geekhack.js
@@ -1,15 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import 'react-image-lightbox/style.css';
 import './Geekhack.css';
-import FadeInSection from "./FadeInSection";
 
 function Geekhack(props) {
     return (
         <div className="geekhack-section">
-            <form action={props.link}>
-                <input id="geekhack-link" type="submit" value="VISIT THE GEEKHACK INTEREST CHECK HERE"/>
-            </form>
+            <a href={props.link} id="geekhack-link">VISIT THE GEEKHACK INTEREST CHECK HERE</a>
         </div>
     )
 }
